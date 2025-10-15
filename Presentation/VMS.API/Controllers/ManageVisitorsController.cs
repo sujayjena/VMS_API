397277935ae66a84870ba24975ad0ee1c4768d24
nazbullah-netswift <nazbullah.netswift2021@gmail.com>
2025-10-15 15:42:55 +0530

    sms on rescheduled

--- Presentation/VMS.API/Controllers/ManageVisitorsController.cs
+++ Presentation/VMS.API/Controllers/ManageVisitorsController.cs
@@ -99,6 +99,20 @@ namespace VMS.API.Controllers
                 }
             }
 
+            //get previous visitstartdate
+            DateTime? prevVisitStartDate = null;
+            var vVisitNumber = "";
+
+            if (parameters.Id > 0)
+            {
+                var vVisitor = await _manageVisitorsRepository.GetVisitorsById(parameters.Id);
+                if (vVisitor != null)
+                {
+                    prevVisitStartDate = vVisitor.VisitStartDate;
+                    vVisitNumber = vVisitor.VisitNumber;
+                }
+            }
+
             int result = await _manageVisitorsRepository.SaveVisitors(parameters);
 
             if (result == (int)SaveOperationEnums.NoRecordExists)
@@ -339,6 +353,46 @@ namespace VMS.API.Controllers
                 }
                 #endregion
 
+                #region send sms on reschedule 
+                if (parameters.Id > 0)
+                {
+                    if (prevVisitStartDate != parameters.VisitStartDate)
+                    {
+                        var vConfigRef_Search = new ConfigRef_Search()
+                        {
+                            Ref_Type = "SMS",
+                            Ref_Param = "VisitorRescheduled"
+                        };
+
+                        string sSMSTemplateName = string.Empty;
+                        string sSMSTemplateContent = string.Empty;
+                        var vConfigRefObj = _configRefRepository.GetConfigRefList(vConfigRef_Search).Result.ToList().FirstOrDefault();
+                        if (vConfigRefObj != null)
+                        {
+                            sSMSTemplateName = vConfigRefObj.Ref_Value1;
+                            sSMSTemplateContent = vConfigRefObj.Ref_Value2;
+
+                            if (!string.IsNullOrWhiteSpace(sSMSTemplateContent))
+                            {
+                                //Replace parameter 
+                                sSMSTemplateContent = sSMSTemplateContent.Replace("{#var#}", parameters.VisitorName.ToString());
+                                sSMSTemplateContent = sSMSTemplateContent.Replace("{#var1#}", Convert.ToDateTime(parameters.VisitStartDate).ToString("dd/MM/yyyy"));
+                                sSMSTemplateContent = sSMSTemplateContent.Replace("{#var2#}", Convert.ToDateTime(parameters.VisitStartDate).ToString("hh:mm:ss:tt"));
+                            }
+                        }
+
+                        // Send SMS
+                        var vsmsRequest = new SMS_Request()
+                        {
+                            Ref2_Other = vVisitNumber,
+                            TemplateName = sSMSTemplateName,
+                            TemplateContent = sSMSTemplateContent,
+                            Mobile = parameters.VisitorMobileNo,
+                        };
+                        bool bSMSResult = await _smsHelper.SMSSend(vsmsRequest);
+                    }
+                }
+                #endregion
             }
             return _response;
         }
